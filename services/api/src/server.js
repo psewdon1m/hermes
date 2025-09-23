e155b9b6946dab53bb02136bd9258cb5c7076ce1
psewdon1m <service.psewdon1m@gmail.com>
2025-09-23 23:30:07 +0300

    Версия 1.42 — структура сервисов

--- services/api/src/server.js
+++
@@ -1,9 +0,0 @@
-// services/api/src/server.js
-import { createApp } from './app.js';
-
-const port = 8080;
-const app = createApp();
-
-app.listen(port, () => {
-  console.log(`API on :${port}`);
-});
